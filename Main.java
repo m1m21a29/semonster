f7e7c6ced3377c7530e473358aea1131b71bca98
NaoTakeuchi <m1m21a29@oit.ac.jp>
2021-06-28 13:26:33 +0900

    Player

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("SEMonster!");
+    System.out.println("SEMonsterr!");
     Monster monster = new Monster();
+    Player player = new Player();
     System.out.println(monster.toString());
+    player.drawMonsters();
+    player.showDeck();
   }
 }
